14a3755da46e5d8df4752faaba01f9ca6d0f0851
Mario Lazzari <mario.lazzari@gmail.com>
2022-10-18 22:22:42 +0200

    Done lesson 1

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let myName = "Mario";
+console.log(myName);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+let myName = "Mario";
+console.log(myName);
+
+let a: number = 12;
+let b: number = 6;
+let c = 2;
+
+console.log(a / b);
+console.log(c * b);
