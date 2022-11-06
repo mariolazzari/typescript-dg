0d558cd04218f969c74b0d5de52cd1b44edb9a0a
Mario Lazzari <mario.lazzari@gmail.com>
2022-11-06 11:10:31 +0100

    Done  functions

--- 04_Functions/build/js/main.js
+++ 04_Functions/build/js/main.js
@@ -26,3 +26,69 @@ const div = function (a, b) {
 };
 logMsg(div(3, 4));
 logMsg(div(5, 0));
+// optional params
+const addAll = (a, b, c) => {
+    // tyoe guard
+    if (c) {
+        return a + b + c;
+    }
+    return a + b;
+};
+logMsg(addAll(1, 2, 3));
+logMsg(addAll(1, 2));
+// default values
+const sumAll = (a = 0, b, c = 0) => {
+    return a + b + c;
+};
+logMsg(sumAll(1, 2, 3));
+logMsg(sumAll(1, 2));
+logMsg(sumAll(undefined, 2));
+// Rest params
+const total = (a = -10, ...nums) => {
+    return a + nums.reduce((total, num) => (total += num));
+};
+logMsg(total(1, 2, 3, 4));
+logMsg(total(undefined, 2, 3, 4));
+// never type: only for errors!
+const createErr = (msg) => {
+    throw new Error(msg);
+};
+// avoid infinite loop: check return type
+const infinity = () => {
+    let i = 0;
+    // now return is never
+    while (true) {
+        i++;
+        // now return tyoe is void
+        if (i > 100) {
+            break;
+        }
+    }
+};
+// custom guard types
+const isNumber = (value) => {
+    return typeof value === "number";
+};
+const isString = (value) => {
+    return typeof value === "boolean";
+};
+const numberOrString = (value) => {
+    /*
+    switch (typeof value) {
+      case "number":
+        return "number";
+  
+      case "string":
+        return " string";
+  
+      default:
+        return createErr("type not supported");
+    }*/
+    if (isNumber(value)) {
+        return "number";
+    }
+    if (isString(value)) {
+        return "string";
+    }
+    return createErr("Type not supported");
+};
