47fe3be18960bbf9a49c45b60eea2315db93ca47
Mario Lazzari <mario.lazzari@gmail.com>
2022-11-06 10:36:36 +0100

    Done function types

--- 04_Functions/build/js/main.js
+++ 04_Functions/build/js/main.js
@@ -1,1 +1,28 @@
 "use strict";
+// littreral types
+let myName;
+let username;
+username = "Mariarosa";
+// functions
+const add = (a, b) => {
+    return a + b;
+};
+// void
+const logMsg = (msg) => {
+    console.log(msg);
+};
+logMsg("Ciao");
+logMsg(add(1, 2));
+const sub = function (a, b) {
+    return a - b;
+};
+const mult = (a, b) => a * b;
+logMsg(mult(2, 2));
+const div = function (a, b) {
+    if (b === 0) {
+        return 0;
+    }
+    return a / b;
+};
+logMsg(div(3, 4));
+logMsg(div(5, 0));
--- 04_Functions/main.ts
+++ 04_Functions/main.ts
@@ -15,3 +15,39 @@ type UserId = StringOrNumber;
 let myName: "Mario";
 let username: "Mario" | "Mariarosa";
 username = "Mariarosa";
+
+// functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+// void
+const logMsg = (msg: any): void => {
+  console.log(msg);
+};
+
+logMsg("Ciao");
+logMsg(add(1, 2));
+
+const sub = function (a: number, b: number): number {
+  return a - b;
+};
+
+// function definition
+type MathFunction = (a: number, b: number) => number;
+const mult: MathFunction = (a, b) => a * b;
+logMsg(mult(2, 2));
+
+//function interface
+interface IMathFunction {
+  (a: number, b: number): number;
+}
+
+const div: IMathFunction = function (a, b) {
+  if (b === 0) {
+    return 0;
+  }
+  return a / b;
+};
+logMsg(div(3, 4));
+logMsg(div(5, 0));
