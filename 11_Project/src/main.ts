402c256a91bef2f425a03d1b9fd506595e4377fd
Mario Lazzari <mario.lazzari@gmail.com>
2022-11-27 14:03:47 +0100

    Done

--- 11_Project/src/main.ts
+++ 11_Project/src/main.ts
@@ -1,1 +1,50 @@
 import "./css/style.css";
+import FullList from "./model/FullList";
+import ListItem from "./model/ListItem";
+import ListTemplate from "./templates/ListTemplate";
+
+const initApp = () => {
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
+
+  // form
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
+
+  // on form sibmit event handler
+  itemEntryForm.addEventListener("submit", (e: SubmitEvent): void => {
+    e.preventDefault();
+
+    // text
+    const input = document.getElementById("newItem") as HTMLInputElement;
+    const newEntryTest = input.value.trim();
+    if (!newEntryTest.length) {
+      return;
+    }
+
+    // compute new item id
+    const itemId = fullList.list.length
+      ? +fullList.list[fullList.list.length - 1].id + 1
+      : 1;
+
+    // create new item and update list
+    const newItem = new ListItem(itemId.toString(), newEntryTest);
+    fullList.addItem(newItem);
+    template.render(fullList);
+  });
+
+  // clear items button
+  const clearItems = document.getElementById(
+    "clearItemsButton"
+  ) as HTMLButtonElement;
+  clearItems.addEventListener("click", () => {
+    fullList.clear();
+    template.clear();
+  });
+
+  fullList.load();
+  template.render(fullList);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
